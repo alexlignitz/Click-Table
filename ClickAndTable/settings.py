e16a6b0ff68795a1947dba2078be60097884b2d5
alexlignitz <50802319+alexlignitz@users.noreply.github.com>
2021-05-31 18:05:19 +0200

    Update settings.py

--- ClickAndTable/settings.py
+++ ClickAndTable/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g+lz9m_x2w37q@lj(rjgzyesd$8yzn64au*hr(9!b4^9r_z4%-'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
